d40ffb5d7783bb291176b89832d83fcc0a724ebf
Jared <jbeckersalax9@aol.com>
2023-03-28 14:55:02 -0400

    edited build file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # Build commands for front end to create the production build
 rm -rf public
-npm install --prefix vite-project/ && npm run build --vite-project/
+npm run build --vite-project/
 cp -a client/build/. public/
 
 # Build commands for back end
